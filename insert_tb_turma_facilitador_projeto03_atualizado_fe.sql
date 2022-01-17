0592761a5cbf63a69fb0e364eaafd8d9d53e1e28
Fernanda Oliveira <94566676+FernandaBz@users.noreply.github.com>
2022-01-17 16:35:54 -0300

    Add files via upload
    add insert tb_turma_facilitador

---
+++ insert_tb_turma_facilitador_projeto03_atualizado_fe.sql
@@ -0,0 +1,43 @@
+--tb_turma_facilitador
+
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador) 
+	VALUES (12, 'fs001');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)     
+	VALUES (13, 'fs002');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)  
+	VALUES (14, 'fs003');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador) 
+	VALUES (12, 'ft014');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (13, 'ft024');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (13, 'ft034');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (14, 'ft044');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (14, 'ft054');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (0, 'ft064');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES (15,'fs005');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES (15,'ft015');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES ((14,15),'m016');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES (13,'m026');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES (12,'m006');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (15,'ft007');
+
+
+
+
+
+
+
+
+
+
+
