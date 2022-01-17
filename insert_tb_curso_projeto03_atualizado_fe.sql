31da0b32f49e784af498de61c10e89504d1a02bc
Fernanda Oliveira <94566676+FernandaBz@users.noreply.github.com>
2022-01-17 17:00:12 -0300

    Add files via upload
    arquivo com insert tb_curso

---
+++ insert_tb_curso_projeto03_atualizado_fe.sql
@@ -0,0 +1,12 @@
+--tb_curso
+
+INSERT INTO tb_curso (id_curso, nome) 
+	VALUES ('1', 'Web Dev');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)     
+	VALUES ('2', 'Data Analytics');
+
+
+
+
+
+
