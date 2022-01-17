94d0f8ec009c9cd4f55e984dfe83092b47bac271
Fernanda Oliveira <94566676+FernandaBz@users.noreply.github.com>
2022-01-17 16:33:42 -0300

    Add files via upload
    incluidos arquivos para insert das tabelas tb_turma_facilitador e tb_turma

---
+++ insert_tb_turma_facilitador_projeto03_atualizado_fe.sql
@@ -0,0 +1,43 @@
+--tb_turma_facilitador
+
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador) 
+	VALUES (12, 'fs001');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)     
+	VALUES (13, 'fs002');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)  
+	VALUES (14, 'fs003');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador) 
+	VALUES (12, 'ft014','R5012', 'tech');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (13, 'ft024','R45011', 'tech');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (13, 'ft034','R5010', 'tech');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (14, 'ft044','R5009','tech');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (14, 'ft054','R5008','tech');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (0, 'ft064','R5007','tech');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES (15,'fs005','R5006','soft');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES (15,'ft015','R5005','tech');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES ((14,15),'m016','R5004','monitor');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES (13,'m026','R5003','monitor');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
+	VALUES (12,'m006','R5002','monitor');
+INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
+	VALUES (15,'ft007','R5001','tech');
+
+
+
+
+
+
+
+
+
+
+
---
+++ insert_tb_turma_projeto03_atualizado_fe.sql
@@ -0,0 +1,32 @@
+-- insert 15 turmas
+
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2020-01-01', '2020-06-01', 1, 'macaco');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2020-06-01', '2020-12-01', 2, 'elefante');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-01-01', '2021-06-01', 3, 'cachorro');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-01-05', '2021-06-05', 4, 'borboleta');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-01-06', '2021-06-06', 5, 'avestruz');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-01-07', '2021-06-07', 6, 'cabra');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-01-08', '2021-06-08', 7, 'carneiro');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-01-09', '2021-06-09', 8, 'camelo');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-01-10', '2021-06-10', 9, 'cobra');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-01-11', '2021-06-11', 10, 'coelho');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-01-12', '2021-06-12', 11, 'cavalo');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-10-15', '2022-04-15', 12, 'aguia');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-12-10', '2022-06-10', 13, 'galo');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-12-11', '2022-06-11', 14, 'gato');
+INSERT INTO tb_turma(data_inicio, data_fim, id_curso, animal) 
+	VALUES('2021-12-11', '2022-06-11', 15, 'girafa');
