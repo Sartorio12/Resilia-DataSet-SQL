7641567c8e5b2ce9531135f81756875cc2bf4480
Fernanda Oliveira <94566676+FernandaBz@users.noreply.github.com>
2022-01-17 16:49:45 -0300

    Update insert_tb_turma_facilitador_projeto03_atualizado_fe.sql

--- insert_tb_turma_facilitador_projeto03_atualizado_fe.sql
+++ insert_tb_turma_facilitador_projeto03_atualizado_fe.sql
@@ -1,35 +1,38 @@
 --tb_turma_facilitador
 
+
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador) 
-	VALUES (12, 'fs001');
+	VALUES ('t012', 'fs001');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)     
-	VALUES (13, 'fs002');
+	VALUES ('t014', 'fs002');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)  
-	VALUES (14, 'fs003');
+	VALUES ('t013', 'fs003');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador) 
-	VALUES (12, 'ft014');
+	VALUES ('t015', 'ft014');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
-	VALUES (13, 'ft024');
+	VALUES ('t012', 'ft024');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
-	VALUES (13, 'ft034');
+	VALUES ('t013', 'ft034');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
-	VALUES (14, 'ft044');
+	VALUES ('t014', 'ft044');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
-	VALUES (14, 'ft054');
+	VALUES (' ', 'ft054');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
-	VALUES (0, 'ft064');
+	VALUES ('t012', 'ft064');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
-	VALUES (15,'fs005');
+	VALUES ('t015','fs005');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
-	VALUES (15,'ft015');
+	VALUES ('t013','ft015');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
-	VALUES ((14,15),'m016');
+	VALUES ('t014','m016');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
-	VALUES (13,'m026');
+	VALUES ('t015','m026');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)   
-	VALUES (12,'m006');
+	VALUES ('t012','m006');
 INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)    
-	VALUES (15,'ft007');
+	VALUES ('t014','ft007');
+
+
 
 
 
