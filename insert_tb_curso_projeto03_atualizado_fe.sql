4cd29ca5db71d9133bc830aefc3272b53468a46d
Fernanda Oliveira <94566676+FernandaBz@users.noreply.github.com>
2022-01-17 17:00:51 -0300

    Update insert_tb_curso_projeto03_atualizado_fe.sql

--- insert_tb_curso_projeto03_atualizado_fe.sql
+++ insert_tb_curso_projeto03_atualizado_fe.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO tb_curso (id_curso, nome) 
 	VALUES ('1', 'Web Dev');
-INSERT INTO tb_turma_facilitador (id_turma, id_facilitador)     
+INSERT INTO tb_curso (id_curso, nome)    
 	VALUES ('2', 'Data Analytics');
 
 
