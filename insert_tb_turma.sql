1e2fd38a81c4b34b9e760b2b43e46b47774b071f
Pedro Andre <65923746+pedroandre1712@users.noreply.github.com>
2022-01-18 14:36:04 -0300

    Update insert_tb_turma.sql

--- insert_tb_turma.sql
+++ insert_tb_turma.sql
@@ -1,32 +1,32 @@
 -- insert 14 turmas
 
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2020-01-01', '2020-06-01', 1, 'macaco', 0);
+	VALUES(1, '2020-01-01', '2020-06-01', 1, 'macaco', 0);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2020-06-01', '2020-12-01', 1, 'elefante', 0);
+	VALUES(2, '2020-06-01', '2020-12-01', 1, 'elefante', 0);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-01-01', '2021-06-01', 2, 'cachorro', 1);
+	VALUES(3, '2021-01-01', '2021-06-01', 2, 'cachorro', 1);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-01-05', '2021-06-05', 1, 'borboleta', 1);
+	VALUES(4, '2021-01-05', '2021-06-05', 1, 'borboleta', 1);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-01-06', '2021-06-06', 1, 'avestruz', 1);
+	VALUES(5, '2021-01-06', '2021-06-06', 1, 'avestruz', 1);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-01-07', '2021-06-07', 2, 'cabra', 1);
+	VALUES(6, '2021-01-07', '2021-06-07', 2, 'cabra', 1);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-01-08', '2021-06-08', 1, 'carneiro', 1);
+	VALUES(7, '2021-01-08', '2021-06-08', 1, 'carneiro', 1);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-01-09', '2021-06-09', 1, 'camelo', 1);
+	VALUES(8, '2021-01-09', '2021-06-09', 1, 'camelo', 1);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-01-10', '2021-06-10', 2, 'cobra', 1);
+	VALUES(9, '2021-01-10', '2021-06-10', 2, 'cobra', 1);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-01-11', '2021-06-11', 1, 'coelho', 1);
+	VALUES(10, '2021-01-11', '2021-06-11', 1, 'coelho', 1);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-01-12', '2021-06-12', 1, 'cavalo', 1);
+	VALUES(11, '2021-01-12', '2021-06-12', 1, 'cavalo', 1);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-10-15', '2022-04-15', 2, 'aguia', 0);
+	VALUES(12, '2021-10-15', '2022-04-15', 2, 'aguia', 0);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-12-10', '2022-06-10', 1, 'galo', 0);
+	VALUES(13, '2021-12-10', '2022-06-10', 1, 'galo', 0);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2021-12-11', '2022-06-11', 1, 'gato', 0);
+	VALUES(14, '2021-12-11', '2022-06-11', 1, 'gato', 0);
 INSERT INTO tb_turma(id_turma, data_inicio, data_fim, id_curso, animal, modulo) 
-	VALUES('2022-01-02', '2022-07-02', 2, 'girafa', 0);
+	VALUES(15, '2022-01-02', '2022-07-02', 2, 'girafa', 0);
