4683d1861d3120859060e4f626848ff606c513b3
Pedro Andre <65923746+pedroandre1712@users.noreply.github.com>
2022-01-18 14:50:45 -0300

    Update queries.sql
    Fixed some bugs

--- queries.sql
+++ queries.sql
@@ -9,25 +9,28 @@ INNER JOIN tb_curso cu
 ON cu.id_curso = tu.id_curso;
 
 -- Selecionar quais pessoas facilitadoras atuam em mais de uma turma.
-select count(tf.id_facilitador) qntd, fa.nome  from tb_turma_facilitador tf
-INNER JOIN tb_facilitador fa
-ON tf.id_facilitador = fa.id_facilitador
-GROUP BY fa.id_facilitador
+select tc.nome ,count(tf.id_facilitador) qntd  from tb_turma_facilitador tf
+INNER JOIN tb_facilitador fa ON tf.id_facilitador = fa.id_facilitador
+INNER JOIN tb_colaborador tc ON tc.id_colaborador = fa.id_colaborador
+GROUP BY tc.nome
 HAVING count(tf.id_facilitador)>1;
 
 -- Quantos alunos foram formados em WebDev?
-select count(id_aluno) from tb_aluno ta
+select tt.id_curso, count(id_aluno) from tb_aluno ta
 inner join tb_turma tt ON ta.id_turma = tt.id_turma
-having tt.modulo = 0;
+where tt.modulo = 0
+group by tt.id_curso
+having tt.id_curso = 1
+;
 
--- Quantas turmas comecaram a partir de 2020 ?
+-- Quantas turmas comecaram a partir de 2021 ?
 select count(id_turma) from tb_turma
-where data_inicio >= '01-01-2020';
+where data_inicio >= '01-01-2021';
 
 
 -- Qual turma possui a maior quantidade de facilitadores? 
-select id_turma, count(id_facilitador) from tb_turma tt
+select tt.id_turma, count(id_facilitador) from tb_turma tt
 inner join tb_turma_facilitador ttf ON tt.id_turma = ttf.id_turma
-group by id_turma
+group by tt.id_turma
 order by count(id_facilitador) desc
 limit 1;
