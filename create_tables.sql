0752f0e921e97c53835e66ea06cb564323109114
Pedro Andre <65923746+pedroandre1712@users.noreply.github.com>
2022-01-19 17:26:05 -0300

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -7,7 +7,8 @@ CREATE TABLE tb_aluno (
     data_nascimento DATE NULL, 
     sexo varchar(1) NULL,
     email varchar(50) NULL,
-    ativo integer default 1
+    ativo integer default 1`,
+    PRIMARY KEY (id_aluno)
 );
 
 CREATE TABLE tb_turma(
